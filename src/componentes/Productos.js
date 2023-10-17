0e7ad5527527572bb89b628169b9f3d9f1511e0c
Gonzalo Carcamo <gon.cmo.dev@gmail.com>
2023-10-17 16:53:44 -0300

    Estilos Generales

--- src/componentes/Productos.js
+++ src/componentes/Productos.js
@@ -1,11 +1,19 @@
 import { Component } from 'react'
 import Producto from './Producto.js'
 
+const estilos = {
+    productos: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+}
+
 class Productos extends Component {
     render(){
         const { productos, agregarAlCarro } = this.props
         return(
-            <div>
+            <div style={estilos.productos}>
                 {productos.map(producto => 
                     <Producto 
                         agregarAlCarro={agregarAlCarro}
